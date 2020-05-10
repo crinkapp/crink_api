d003efe266d700310649848e5417770e64c828a4
Eudrey <eudreybandelaria@gmail.com>
2020-05-10 16:49:17 +0200

    added cors to app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 //Middleweares
+app.use(cors());
 app.use(bodyParser.json());
 
 // Import Routes

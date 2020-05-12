244e049d0cc534207ae06838eccbcc275403ff23
Eudrey <eudreybandelaria@gmail.com>
2020-05-12 18:43:04 +0200

    fixed correct name of value and implemented html file in sent newsletter

--- routes/newsletters.js
+++ routes/newsletters.js
@@ -20,8 +20,8 @@ router.get('/emails', async (req, res) => {
 router.post('/sendemail', async (req, res) => {
     const mail = req.body.email;
     const newsletters = new Newsletters({
-        email: mail,
-        activate: 1
+        newsletter_email: mail,
+        newsletter_activate: 1
     });
 
     let transporter = nodemailer.createTransport({
@@ -41,21 +41,7 @@ router.post('/sendemail', async (req, res) => {
         to: mail,
         subject: "Merci pour votre inscription 🎉",
         text: "Toute l'équipe Crink vous souhaite la bienvenue !",
-        html: "<div style='color: #282727; margin: 0 auto; width: 27rem; border: 1px solid lightgrey; border-radius: 3px;'>" +
-        "<div style='display: flex; background-color: #BD6D5C; padding-top: 2rem; padding-bottom: 2rem; justify-content: center; align-items: center; width: 100.5%; border-top-left-radius: 3px; border-top-right-radius: 3px;'>" +
-        //"<img src='cid:logo' alt='Crink' height='40' style='display: flex; margin-left: auto; margin-right: auto'></div>" +
-        "<div style='width: 100%; padding-top: 3rem; padding-bottom: 3rem; background-color: #FDFAF5; line-height: 1.4rem;'>" +
-        "<h3 style='text-align: center; margin-bottom: 4rem; font-weight: 600;'>Merci pour votre inscription <span style='margin-left: 0.6rem;'>🎉</span></h3>" +
-        "<div style='padding-left: 2rem; padding-right: 2rem;'>" +
-        "<p style='font-weight: 300;'>Toute l'équipe Crink vous souhaite la bienvenue !</p>" +
-        "<p style='font-weight: 300;'>Le projet est encore en développement et nous vous prions de bien vouloir patienter avant le jour de lancement. Nous comptons sur vous pour en parler à vos amis qui pourraient être intérressés.</p>" +
-        "<p style='font-weight: 300;'>Comme promis, nous vous enverrons un petit message pour vous tenir informé de notre progression. En attendant, n'hésitez pas à faire un tour sur nos réseaux sociaux !</p>" +
-        "<p style='font-weight: 300; margin-top: 3rem; margin-bottom: 0;'>Prenez soins de vous et à bientôt !</p>" +
-        "<span style='font-weight: 700;'>L'équipe Crink</span>" +
-        "<div style='margin: 2rem 0 0;'>" +
-        "<a href='https://www.instagram.com/crinkappli/' target='blank' style='text-decoration: none;'><img src='https://image.flaticon.com/icons/png/512/174/174855.png' alt='Instagram' height='30'></a>" +
-        "<a href='https://www.linkedin.com/company/42421692' target='blank' style='margin-left: 0.6rem; text-decoration: none;'><img src='https://image.flaticon.com/icons/png/512/174/174857.png' alt='Linkedin' height='30'></a>" +
-        "</div></div></div></div>",
+        html: { path: 'templates/newsletter.html' }
     };
 
     transporter.sendMail(mailOptions, (err, data) => {

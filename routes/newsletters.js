cab3cb6d8cc1b36e16d7d8c6aed0850469fb44b2
Eudrey <eudreybandelaria@gmail.com>
2020-05-16 11:36:56 +0200

    removed useless line break

--- routes/newsletters.js
+++ routes/newsletters.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
-
 const  { getAllNewsletters, addNewsletter } = require('../controllers/NewslettersController');
 
 router.get('/emails', getAllNewsletters);

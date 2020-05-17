e8c5cd1c1eaf20c9bf474fef9bb1754d386f4b3d
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-05-17 22:12:34 +0200

    fixed getAll mails request + adjust addNewsletter + add unsubscribe request

--- controllers/NewslettersController.js
+++ controllers/NewslettersController.js
@@ -4,68 +4,101 @@ const hbs = require('nodemailer-express-handlebars');
 const path = require('path');
 require('dotenv').config();
 
-async function getAllNewsletters( req, res) {
+async function getAllNewsletters(req, res) {
     try {
-        const allEmails =  await Newsletters.find();
-        res.json(allEmails);
-
+        const allEmails =  await Newsletters.findAll({raw: true});
+        return res.json(allEmails);
     }
     catch(err) {
-        res.json({ message: err })
+        return res.json(err)
     }
 }
 
-async function addNewsletter( req, res ) {
+async function addNewsletter(req, res ) {
     const mail = req.body.email_newsletters;
-    const newsletters = new Newsletters({
-        email_newsletters: mail,
-        activate_newsletters: 1
-    });
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ionos.fr',
-        port: '465',
-        secure: true,
-        auth: {
-            clientId: process.env.EMAIL_ADDRESS,
-            clientSecret: process.env.EMAIL_PASSWORD,
-            user: process.env.EMAIL_ADDRESS,
-            pass: process.env.EMAIL_PASSWORD
+    Newsletters.findOne({ where: { email_newsletters: mail } })
+    .then((email) => {
+      if(email !== null) {
+        // if already exist
+        try {
+            Newsletters.update({activate_newsletters: true}, {
+                where: {
+                    email_newsletters: email
+                }
+            });
+            return res.json('success')
+    
+        } catch (err) {
+            return res.json(err)
         }
+      } else {
+        // if doesn't exist
+        const newsletters = new Newsletters({
+            email_newsletters: mail,
+            activate_newsletters: 1
+        });
+        let transporter = nodemailer.createTransport({
+            host: 'smtp.ionos.fr',
+            port: '465',
+            secure: true,
+            auth: {
+                clientId: process.env.EMAIL_ADDRESS,
+                clientSecret: process.env.EMAIL_PASSWORD,
+                user: process.env.EMAIL_ADDRESS,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        });
+        const handlebarOptions = {
+            viewEngine: {
+                extname: '.handlebars',
+                partialsDir: path.join(__dirname, '../views'),
+                layoutsDir: path.join(__dirname, '../views'),
+                defaultLayout: 'newsletter'
+            },
+            viewPath: path.join(__dirname, '../views'),
+            extName: '.handlebars',
+        };
+        transporter.use('compile', hbs(handlebarOptions));
+        const mailOptions = {
+            from: process.env.EMAIL_ADDRESS,
+            to: mail,
+            subject: "Merci pour votre inscription 🎉",
+            template: 'newsletter'
+        };
+        transporter.sendMail(mailOptions, (err, data) => {
+            if (err) {
+                return res.json(err);
+            }
+            newsletters.save().then(data => {
+                return res.json(data);
+            }).catch(err => {
+                console.log(err);
+                return;
+            })
+        });
+      }
     });
-    const handlebarOptions = {
-        viewEngine: {
-            extname: '.handlebars',
-            partialsDir: path.join(__dirname, '../views'),
-            layoutsDir: path.join(__dirname, '../views'),
-            defaultLayout: 'newsletter'
-        },
-        viewPath: path.join(__dirname, '../views'),
-        extName: '.handlebars',
-    };
-    transporter.use('compile', hbs(handlebarOptions));
-    const mailOptions = {
-        from: process.env.EMAIL_ADDRESS,
-        to: mail,
-        subject: "Merci pour votre inscription 🎉",
-        template: 'newsletter'
-    };
-    transporter.sendMail(mailOptions, (err, data) => {
-        if (err) {
-            return res.json(err);
-        }
-        newsletters.save().then(data => {
-            return res.json(data);
-        }).catch(err => {
-            console.log(err);
-            return;
-        })
-    });
+}
+
+async function unsubscribeUser(req, res) {
+    const userEmail = req.body.email_newsletters;
+    try {
+        await Newsletters.update({activate_newsletters: false}, {
+            where: {
+                email_newsletters: userEmail
+            }
+        });
+        return res.json('Successfully unsubscribed')
+
+    } catch (err) {
+        return res.json(err)
+    }
 }
 
 module.exports = {
     getAllNewsletters,
     addNewsletter,
-
+    unsubscribeUser
 };
 
 

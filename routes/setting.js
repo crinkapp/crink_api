9656d7606a28473a7081e36766a3ea0272609e98
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-05-21 01:28:02 +0200

    Revert "Fixed img issues in gmail"

--- routes/setting.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const  { addSetting } = require('../controllers/SettingController');
-
-
-router.post('/', addSetting );
-module.exports = router;

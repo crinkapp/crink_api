86aa666d711325672be383e24e08659b17345fbb
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-06-12 00:53:53 +0200

    fixed Add User function

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,8 +1,5 @@
 const { User } = require('../sequelize');
-const { Newsletters } = require('../sequelize');
-const nodemailer = require('nodemailer');
-const hbs = require('nodemailer-express-handlebars');
-const path = require('path');
+const  { sendEmail } = require('../controllers/NewslettersController');
 require('dotenv').config();
 
 async function getAllUsers(req, res) {
@@ -24,61 +21,14 @@ async function addUser(req, res) {
     user.save();
     if (req.body.newsletter_user) {
         const mail = req.body.email_user;
-        await Newsletters.findOne({ where: { email_newsletters: mail } })
-        .then((email) => {
-        if(email !== null) {
-            // if already exist
+        try {
+            sendEmail(mail, res);
+        } catch(err) {
             return res.json('Vous êtes déjà inscrit à la newsletter…');
-        } else {
-            // if doesn't exist
-            const newsletters = new Newsletters({
-                email_newsletters: mail,
-            });
-            let transporter = nodemailer.createTransport({
-                host: 'smtp.ionos.fr',
-                port: '465',
-                secure: true,
-                auth: {
-                    clientId: process.env.EMAIL_ADDRESS,
-                    clientSecret: process.env.EMAIL_PASSWORD,
-                    user: process.env.EMAIL_ADDRESS,
-                    pass: process.env.EMAIL_PASSWORD
-                }
-            });
-            const handlebarOptions = {
-                viewEngine: {
-                    extname: '.handlebars',
-                    partialsDir: path.join(__dirname, '../views'),
-                    layoutsDir: path.join(__dirname, '../views'),
-                    defaultLayout: 'newsletter'
-                },
-                viewPath: path.join(__dirname, '../views'),
-                extName: '.handlebars',
-            };
-            transporter.use('compile', hbs(handlebarOptions));
-            const mailOptions = {
-                from: process.env.EMAIL_ADDRESS,
-                to: mail,
-                subject: "Merci pour votre inscription 🎉",
-                template: 'newsletter',
-                context: {
-                    email: mail
-                }
-            };
-            transporter.sendMail(mailOptions, (err, data) => {
-                if (err) {
-                    return res.json(err);
-                }
-                newsletters.save().then(data => {
-                    return res.json(data);
-                }).catch(err => {
-                    return res.json(err);
-                })
-            });
         }
-        });
+    } else {
+        return res.json(user);
     }
-    return res.json(user);
 }
 
 async function removeUser(req, res) {

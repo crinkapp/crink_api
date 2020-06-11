3c95c673a3987fda04c1569c6f175ec1c9e006ca
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-06-12 01:16:58 +0200

    removed useless import

--- models/LikeUserModel.js
+++ models/LikeUserModel.js
@@ -1,12 +1,9 @@
-const user = require('./UsersModel');
-//const publication = require('./PublicationModel');
 const LikeUserModel = (sequelize, type) => {
     return sequelize.define('like_user',{
         id: {
             primaryKey: true,
             type: type.INTEGER
         },
-
     })
 };
 

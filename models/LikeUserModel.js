1564955a0c19989a83c38f2630996889989ddcb0
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-06-11 22:51:41 +0200

    replaced id_like_user

--- models/LikeUserModel.js
+++ models/LikeUserModel.js
@@ -2,7 +2,7 @@ const user = require('./UsersModel');
 const publication = require('./PublicationModel');
 const LikeUserModel = (sequelize, type) => {
     return sequelize.define('like_user',{
-        id_like_user: {
+        id: {
             type: type.INTEGER
         },
 

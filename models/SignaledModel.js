c0a909629415c5d58c50e0b15de911d972153b72
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-06-12 01:37:23 +0200

    added content in SignaledModel

---
+++ models/SignaledModel.js
@@ -0,0 +1,22 @@
+const sequelize = require("../sequelize");
+
+const SignaledModel = (sequelize, type) => {
+    return sequelize.define('signaled',{
+        id: {
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        type_signaled: {
+            type: type.STRING,
+            allowNull: true
+        },
+        comment_signaled: {
+            type: type.TEXT,
+            allowNull: true
+        },
+
+    })
+};
+
+module.exports = SignaledModel;

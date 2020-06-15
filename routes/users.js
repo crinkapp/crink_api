c4b419b002b93d6373e4b75fbc263e814bbfa1fe
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-06-15 23:43:39 +0200

    added route for getting a user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const  { getAllUsers, addUser, removeUser } = require('../controllers/UsersController');
+const  { getAllUsers, addUser, removeUser, getUser } = require('../controllers/UsersController');
 
 /**
  * @swagger
@@ -27,9 +27,17 @@ const  { getAllUsers, addUser, removeUser } = require('../controllers/UsersContr
  *      responses:
  *          200:
  *              description: Request went well
+ *  post:
+ *      description: Get a user from User table using email & password
+ *      produces:
+ *          - application/json
+ *      responses:
+ *          200:
+ *              description: Request went well
  */
 router.get('/users', getAllUsers);
 router.post('/user', addUser);
 router.put('/user', removeUser);
+router.post('/getuser', getUser);
 
 module.exports = router;

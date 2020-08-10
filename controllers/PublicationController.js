44682caad38bfbb51a0a59daf1ae887208bcbf1d
Aïssatou DIOP <aissatou.diop@dagence.com>
2020-08-10 19:12:46 +0200

    update addPublication : pulication's tags added

--- controllers/PublicationController.js
+++ controllers/PublicationController.js
@@ -1,27 +1,26 @@
-const { Publication } = require('../sequelize');
+const {Publication, PublicationTag} = require('../sequelize');
 
-async function getAllPublications(req, res){
+async function getAllPublications(req, res) {
 
     try {
-        const allUPublications =  await Publication.findAll({raw: true});
+        const allUPublications = await Publication.findAll({raw: true});
         return res.json(allUPublications);
-    }
-    catch(err) {
+    } catch (err) {
         return res.status(400).send('No publications found');
     }
 }
 
-async function getAllPublicationByUser(req, res){
+async function getAllPublicationByUser(req, res) {
 
     const user_id = res.locals.id_user;
-    if(user_id){
+    if (user_id) {
         try {
             const user_publications = await Publication.findAll({
                 where: {userId: user_id}
             });
             return res.json(user_publications);
 
-        } catch(err) {
+        } catch (err) {
             return res.status(400).send("Can't find user's publications");
         }
 
@@ -31,19 +30,19 @@ async function getAllPublicationByUser(req, res){
     }
 }
 
-async function getUserPublicationById(req, res){
+async function getUserPublicationById(req, res) {
 
     const user_id = res.locals.id_user;
     const pub_id = req.body.id;
 
-    if(user_id && pub_id){
+    if (user_id && pub_id) {
         try {
             const user_publication = await Publication.findOne({
                 where: {userId: user_id, id: pub_id}
             });
             return res.json(user_publication);
 
-        } catch(err) {
+        } catch (err) {
             return res.status(400).send("Can't find user's publications");
         }
 
@@ -51,9 +50,6 @@ async function getUserPublicationById(req, res){
         return res.status(400).send("Unknow user id or id publication");
 
     }
-
-
-
 }
 
 async function addPublication(req, res) {
@@ -62,64 +58,88 @@ async function addPublication(req, res) {
 
     const title = req.body.title;
     const content = req.body.content;
-    const media = req.body.media;
+    const media = req.body.media; // array with : name, type, tmp_name, error, size
     const status = 'active';
+    const hashtags = req.body.hashtags;
+    //const nameImg =  media.tmp_name le tmp_name de l'image'
+    //const extensionImg = media.type  extension de l'image
+    //const path = "https://crink/publication/img/";
+    //const md5 = md5(nameImg);
+    //const lastPath = path+user_id+"/"+nameImg+"."+extensionImg;
+    try {
+        // check if user exist
+        if (user_id) {
 
-        try{
-            if(user_id) {
-                const publication =  await Publication.create({
-                    title_publication: title,
-                    content_publication: content,
-                    path_media_publication: media,
-                    status_publication: status,
-                    userId: user_id
+            // create publication and save in db
+            const publication = await Publication.create({
+                title_publication: title,
+                content_publication: content,
+                path_media_publication: media, // upload lastPath
+                status_publication: status,
+                userId: user_id
 
+            });
+            //  save publication and get publication's id
+            let publicationId = {};
+            await publication.save().then(data => {
+                publicationId = data.dataValues.id;
+            });
+
+            // save publication's tags in db
+            for (let i = 0; i < hashtags.length; i++) {
+                const tags = await PublicationTag.create({
+                    publicationId: publicationId,
+                    tagId: hashtags[i].tagId
                 });
-                publication.save;
-                return res.json("Publication successuflly added")
-
-            }else{
-                return res.json("can't find user")
+                console.log(tags);
+                tags.save()
             }
-            }catch(err){
-                return res.status(400).send("Something went wrong")
+            return res.json("success")
+
+            // if user does not exist
+        } else {
+            return res.json("can't find user")
         }
+    } catch (err) {
+        return res.status(400).send("Something went wrong")
+    }
 }
 
-async function updatePublication(req, res){
+async function updatePublication(req, res) {
 
-        const publicationExist = await Publication.findOne({where: {id: req.body.id}});
-        const new_values = req.body;
-        if(publicationExist && req.body) {
-            await Publication.update(
-                new_values, {
-                    where: {id: req.body.id}
-                });
-            return res.json('Success update');
-        }else{
-            return res.json('Unknown id or no data sent to update');
+    const publicationExist = await Publication.findOne({where: {id: req.body.id}});
+    const new_values = req.body;
+    if (publicationExist && req.body) {
+        await Publication.update(
+            new_values, {
+                where: {id: req.body.id}
+            });
+        return res.json('Success update');
+    } else {
+        return res.json('Unknown id or no data sent to update');
 
-        }
+    }
 }
 
-async function deletePublication(req, res){
+async function deletePublication(req, res) {
 
     const user_id = res.locals.id_user;
 
     const findPublication = await Publication.findOne({where: {id: req.body.id}});
-    if(user_id && findPublication ){
-        try{
+    if (user_id && findPublication) {
+        try {
             await Publication.destroy({where: {id: req.body.id}});
             return res.json(' Publication Successfully removed');
 
-        }catch (err) {
+        } catch (err) {
             return res.status(400).send('bad request')
         }
-    }else{
+    } else {
         return res.status(400).send("Can't find publication")
     }
 }
-module.exports= {
+
+module.exports = {
     addPublication,
     updatePublication,
     deletePublication,

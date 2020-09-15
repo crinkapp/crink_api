236e10d048d187d38e8a251bc9f37392a4edf57d
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-15 16:10:07 +0200

    added PORT in sequelize and db config

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,8 @@ module.exports = {
     HOST: "us-cdbr-east-02.cleardb.com",
     USER: "bbcbb93c286f10",
     PASSWORD: "f99dd40d",
-    DB: "heroku_726f51a979c2863x"
+    DB: "heroku_726f51a979c2863x",
+    PORT: 3307
   };
 
 

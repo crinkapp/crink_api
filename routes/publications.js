452182c407fb60d0c55b0b745c8443606a3655c5
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-16 21:40:44 +0200

    clean up

--- routes/publications.js
+++ routes/publications.js
@@ -1,22 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { addPublication, updatePublication, deletePublication, getAllPublicationByUser, getAllPublications, getUserPublicationById } = require('../controllers/PublicationController');
-const verifyToken = require('../token/verifyToken');
+const {
+  addPublication,
+  updatePublication,
+  deletePublication,
+  getAllPublicationByUser,
+  getAllPublications,
+  getUserPublicationById,
+  getPublicationByUserTags,
+} = require("../controllers/PublicationController");
+const verifyToken = require("../token/verifyToken");
 
 // GET
-router.get('/user-publication', verifyToken, getUserPublicationById);
-router.get('/all-publications', verifyToken, getAllPublications);
-router.get('/all-user-publications', verifyToken, getAllPublicationByUser);
+router.get("/user-publication", verifyToken, getUserPublicationById);
+router.get("/all-publications", verifyToken, getAllPublications);
+router.get("/all-user-publications", verifyToken, getAllPublicationByUser);
+router.get("/all-publications-user-tags", verifyToken, getPublicationByUserTags);
 
 // POST
-router.post('/add-publication', verifyToken, addPublication);
+router.post("/add-publication", verifyToken, addPublication);
 
 // PUT
-router.put('/update-publication', verifyToken, updatePublication);
+router.put("/update-publication", verifyToken, updatePublication);
 
 // DELETE
-router.delete('/delete-publication', verifyToken, deletePublication);
+router.delete("/delete-publication", verifyToken, deletePublication);
 
-
-
-module.exports= router;
+module.exports = router;

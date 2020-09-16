ee52133549a505285075690ec6b7576cf7f026bc
Aïssatou DIOP <aissatou.diop@dagence.com>
2020-09-16 15:10:57 +0200

    GET all user subscribers ok

--- controllers/SubscriptionController.js
+++ controllers/SubscriptionController.js
@@ -77,7 +77,39 @@ async function getAllSubscriptionByUser(req, res) {
                 attributes: { exclude: ["password_user", "diagnosticId", "settingId",  "createdAt", "updatedAt", "email_user", "birthdate_user", "gender_user","last_name_user","first_name_user"] }
             });
 
-            // return user object with id, username, and path_to_profil_picture
+            // return all the users which userId subscribe to, (username, and path_to_profil_picture)
+            return res.json(users);
+        } catch (err) {
+            return res.status(400).send("Can't find user's subscriptions");
+        }
+    } else {
+        return res.status(400).send("Unknow user id");
+    }
+}
+
+async function getAllSubscribersByUser(req, res) {
+    const user_id = res.locals.id_user;
+    if (user_id) {
+        try {
+            const user_subscribers = await Subscription.findAll({
+                attribute: ["userId"],
+                where: {user_subscription: user_id},
+            });
+
+            let userIds = [];
+            for (let key in user_subscribers) {
+                userIds.push(user_subscribers[key].dataValues.user_subscription);
+            }
+
+            // Get user object by the array of id user
+            const users = await User.findAll({
+                where: {
+                    id: userIds,
+                },
+                attributes: {exclude: ["password_user", "diagnosticId", "settingId", "createdAt", "updatedAt", "email_user", "birthdate_user", "gender_user", "last_name_user", "first_name_user"]}
+            });
+
+            // return all the users who subscribe to userId  (username, and path_to_profil_picture)
             return res.json(users);
         } catch (err) {
             return res.status(400).send("Can't find user's subscriptions");
@@ -90,4 +122,5 @@ async function getAllSubscriptionByUser(req, res) {
 module.exports = {
     addSubscribe,
     getAllSubscriptionByUser,
+    getAllSubscribersByUser,
 };

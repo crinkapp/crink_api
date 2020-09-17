6081c1ce75546c7e1c79453c6855da022d983168
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-18 00:50:58 +0200

    removed useless routes in subscription

--- routes/subscription.js
+++ routes/subscription.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { addSubscribe, getAllSubscriptionByUser, getAllSubscribersByUser, nbSubscriptionsByUserId, nbSubscribersByUserId } = require('../controllers/SubscriptionController');
-const VerifyToken = require('../token/verifyToken');
+const {
+  addSubscribe,
+  getAllSubscriptionByUser,
+  getAllSubscribersByUser,
+  nbSubscriptionsByUserId,
+  nbSubscribersByUserId,
+} = require("../controllers/SubscriptionController");
+const VerifyToken = require("../token/verifyToken");
 
-router.post('/add-subscribe',VerifyToken, addSubscribe);
-router.get('/all-user-subscriptions',VerifyToken, getAllSubscriptionByUser);
-router.get('/all-user-subscribers', VerifyToken, getAllSubscribersByUser);
-router.get('/nb-user-subscriptions', VerifyToken, nbSubscriptionsByUserId,);
-router.get('/nb-user-subscribers', VerifyToken, nbSubscribersByUserId);
-
+router.post("/add-subscribe", VerifyToken, addSubscribe);
+router.get("/all-user-subscriptions", VerifyToken, getAllSubscriptionByUser);
+router.get("/all-user-subscribers", VerifyToken, getAllSubscribersByUser);
 
 module.exports = router;

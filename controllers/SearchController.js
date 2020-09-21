adbf999e62e042173d74f4d5b4bdd1554d5bf1f1
Aïssatou DIOP <aissatou.diop@dagence.com>
2020-09-21 16:36:03 +0200

    add request get tagsSeen

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -7,17 +7,43 @@ const {
 
 async function searchPublicationByTags(req, res) {
 
-    const id = req.body.tag_id;
+    const tagId = req.body.tag_id;
 
-    let publicationsIds ;
-    const research = await PublicationTag.findAll({where: {tagId: id},
-    attributes: ["publicationId"]}).then((result) =>{
+   // return all publications' id where tagId = tag_id
+    await PublicationTag.findAll({where: {tagId},
+    attributes: ["publicationId"]})
+        .then(async(result) =>{
+            // return publications from array of publicationIds
         const publicationIds = result.map((res)=>res.publicationId);
-            return res.json(publicationIds);
+            await Publication.findAll({where: {id: publicationIds}}).then((publications)=>{
+                return res.json(publications)
+            }).catch((err)=>{
+                return res.json(err);
+            })
+        });
+}
 
-    });
+async function searchPublicationByAuthor(req, res){
+    const research_field = req.body.research_field;
+    const val = '%'+research_field+'%';
+    await User.findAll({where:
+            {
+                username:
+                    {
+                        '$like': val
+                    }
+            },
+            attributes: ["userId"]}).then(async(result)=>{
+                const userIds = result.map((res)=>res.userId);
+                await Publication.findAll({where: {id: userIds}}).then((publications)=>{
+                    return res.json(publications)
+                }).catch((err) =>{
+                    return res.json(err)
+                })
 
+    })
 }
 module.exports = {
-    searchPublicationByTags
+    searchPublicationByTags,
+    searchPublicationByAuthor
 };
--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const  { searchPublicationByTags } = require('../controllers/SearchController');
+const  { searchPublicationByTags, searchPublicationByAuthor } = require('../controllers/SearchController');
 
 module.exports = router;
 
 router.get('/search-publication-by-tag', searchPublicationByTags);
+router.get('/search-publication-by-author', searchPublicationByAuthor);

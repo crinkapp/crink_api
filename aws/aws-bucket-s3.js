f4866323d2afdf7265a0e218b171e4d7c9d0ae3c
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-21 17:38:05 +0200

    merged and format code

--- aws/aws-bucket-s3.js
+++ aws/aws-bucket-s3.js
@@ -1,23 +1,23 @@
-const AWS = require('aws-sdk');
-const crypto = require('crypto');
+const AWS = require("aws-sdk");
+const crypto = require("crypto");
 
 //configuring the AWS environment
 AWS.config.update({
   accessKeyId: process.env.AWS_ID,
-  secretAccessKey: process.env.AWS_KEY
+  secretAccessKey: process.env.AWS_KEY,
 });
 
 function uploadMedia(fileName, fileContent, fileType, user_id) {
   const s3 = new AWS.S3();
-  const type = fileType.split('/');
+  const type = fileType.split("/");
   const fileExtension = type[1];
-  const hash = crypto.createHmac('sha256', fileName).digest('hex');
+  const hash = crypto.createHmac("sha256", fileName).digest("hex");
   const userFolder = "user" + user_id + "/";
   const key = Date.now() + hash + "." + fileExtension;
   const params = {
     Bucket: process.env.BUCKET_DEV,
     Body: fileContent,
-    Key: "user/" + userFolder + key
+    Key: "user/" + userFolder + key,
     //'4
   };
   s3.upload(params, function (err, data) {
@@ -34,10 +34,10 @@ function uploadMedia(fileName, fileContent, fileType, user_id) {
   });
 
   const tab = {
-    'path': key,
+    path: key,
   };
-  return tab
+  return tab;
 }
 module.exports = {
-  uploadMedia
-}
+  uploadMedia,
+};

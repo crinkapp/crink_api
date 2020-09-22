b6854700b00e815caaa04649716629d4518923d7
Eudrey Bandelaria <eudreybandelaria@gmail.com>
2020-09-22 15:22:58 +0200

    added role to user model

--- models/UsersModel.js
+++ models/UsersModel.js
@@ -1,51 +1,50 @@
 const UsersModel = (sequelize, type) => {
-    return sequelize.define('user', {
-            id: {
-                type: type.INTEGER,
-                primaryKey: true,
-                autoIncrement: true,
-            },
-            first_name_user: {
-                type: type.STRING,
-                allowNull: true,
-
-            },
-            last_name_user: {
-                type: type.STRING,
-                allowNull: true,
-
-            },
-            gender_user: {
-                type: type.ENUM('Man', 'Woman'),
-                allowNull: true,
-            },
-            birthdate_user: {
-                type: type.DATEONLY,
-                allowNull: true,
-
-            },
-            email_user: {
-                type: type.STRING,
-                allowNull: false,
-                validate: {
-                    isEmail: true,
-                },
-                unique: true
-            },
-            username_user: {
-                type: type.STRING,
-                allowNull: false,
-            },
-            password_user: {
-                type: type.STRING,
-                allowNull: false,
-            },
-            path_profil_picture_user: {
-                type: type.STRING,
-                allowNull: true,
-            },
-        },
-    );
+  return sequelize.define("user", {
+    id: {
+      type: type.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name_user: {
+      type: type.STRING,
+      allowNull: true,
+    },
+    last_name_user: {
+      type: type.STRING,
+      allowNull: true,
+    },
+    gender_user: {
+      type: type.ENUM("Man", "Woman"),
+      allowNull: true,
+    },
+    birthdate_user: {
+      type: type.DATEONLY,
+      allowNull: true,
+    },
+    email_user: {
+      type: type.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+      unique: true,
+    },
+    username_user: {
+      type: type.STRING,
+      allowNull: false,
+    },
+    password_user: {
+      type: type.STRING,
+      allowNull: false,
+    },
+    path_profil_picture_user: {
+      type: type.STRING,
+      allowNull: true,
+    },
+    role_user: {
+      type: type.ENUM("Admin", "Writter", "User"),
+    },
+  });
 };
 
 module.exports = UsersModel;
